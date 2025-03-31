2c7702c6a2acce019326eaaef8441ce2ab0c7870
Bibash Japrel <bibashjaprel111@gmail.com>
2025-03-31 10:59:07 +0545

    basic configuration

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-import expreess from 'express';
-
-const app = expreess();
+import express from 'express';
+import dotenv from 'dotenv';
+const app = express();
+dotenv.config();
 const PORT = process.env.PORT || 3000;
 
+// Set the views directory and view engine
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-}
-);
+  res.render('index'); // Render 'index.ejs' from the 'views' folder
+});
+
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
